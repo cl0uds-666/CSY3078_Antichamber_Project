4254702ca444b08b5079fd00472f6d63a38db488
cl0uds-666 <165782448+cl0uds-666@users.noreply.github.com>
2026-05-25 20:13:27 +0100

    Add per-face normals and Lambert lighting for cubes

--- shaders/PixelShader.hlsl
+++ shaders/PixelShader.hlsl
@@ -1,10 +1,28 @@
+cbuffer ConstantBuffer : register(b0)
+{
+    matrix wvp;
+    matrix world;
+    float3 lightDirection;
+    float padding0;
+    float3 lightColor;
+    float padding1;
+    float3 ambientColor;
+    float padding2;
+};
+
 struct pixelIn
 {
     float4 position : SV_POSITION;
+    float3 worldNormal : NORMAL;
     float4 colour : COLOR;
 };
 
 float4 main(pixelIn input) : SV_TARGET
 {
-    return input.colour;
+    float3 normal = normalize(input.worldNormal);
+    float3 toLight = normalize(-lightDirection);
+    float lambert = max(dot(normal, toLight), 0.0f);
+
+    float3 litColor = input.colour.rgb * (ambientColor + (lightColor * lambert));
+    return float4(saturate(litColor), input.colour.a);
 }
--- shaders/VertexShader.hlsl
+++ shaders/VertexShader.hlsl
@@ -1,17 +1,26 @@
 cbuffer ConstantBuffer : register(b0)
 {
     matrix wvp;
+    matrix world;
+    float3 lightDirection;
+    float padding0;
+    float3 lightColor;
+    float padding1;
+    float3 ambientColor;
+    float padding2;
 };
 
 struct vertexIn
 {
     float3 position : POSITION;
+    float3 normal : NORMAL;
     float4 colour : COLOR;
 };
 
 struct vertexOut
 {
     float4 position : SV_POSITION;
+    float3 worldNormal : NORMAL;
     float4 colour : COLOR;
 };
 
@@ -20,6 +29,7 @@ vertexOut main(vertexIn input)
     vertexOut output;
 
     output.position = mul(float4(input.position, 1.0f), wvp);
+    output.worldNormal = normalize(mul(float4(input.normal, 0.0f), world).xyz);
     output.colour = input.colour;
 
     return output;
